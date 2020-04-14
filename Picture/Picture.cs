488ddbe3384344d38ec73db35b77f9402d15a88b
Schreib Juri <juri@schreib.at>
2020-04-14 10:43:33 +0200

    Inital commit

---
+++ Picture/Picture.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JS.Utils.RazorTemplates.Model
+{
+    public class Picture
+    {
+        public List<Source> Sources { get; set; } = new List<Source>();
+        public string Alt { get; set; }
+        public string Src { get; set; }
+
+    }
+}
---
+++ Picture/Source.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JS.Utils.RazorTemplates.Model
+{
+    public class Source
+    {
+        public string Srcset { get; set; }
+        public string Media { get; set; }
+    }
+}
